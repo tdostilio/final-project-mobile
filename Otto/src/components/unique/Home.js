bb87eab6cf608079dadafaf7147f0f5d53694b35
tdostilio <tdostilio@gmail.com>
2017-10-03 15:03:36 -0400

    got Bill Murray to show up on the screen

--- Otto/src/components/unique/Home.js
+++ Otto/src/components/unique/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Button, View } from 'react-native';
+import { ActivityIndicator, ListView, Button, Text, View, Image } from 'react-native';
 
 class Home extends React.Component {
-  static navigationOptions = {
-    title: 'Home',
-  };
+    static navigationOptions = {
+      title: 'Home',
+    };
+
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View>
+        <Image
+          source={{uri: 'https://vignette2.wikia.nocookie.net/ghostbusters/images/1/11/Bill_Murray.jpeg/revision/latest?cb=20081013140955'}}
+          style={{height: 500, width: 500, borderWidth: 1, borderColor: 'red' }}
+        />
         <Button
         title="Go to Welcome"
         onPress={() =>
@@ -28,9 +33,9 @@ class Home extends React.Component {
         }
         />
       </View>
-    );
+    )
   }
-}
+};
 
 export default Home;
 
