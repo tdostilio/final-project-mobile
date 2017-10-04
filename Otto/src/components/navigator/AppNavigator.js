08b8982009e22eee28d8d12b68c7075833f98c35
James Hong <gnohj21@gmail.com>
2017-10-04 19:10:25 -0400

    add Contacts route

--- Otto/src/components/navigator/AppNavigator.js
+++ Otto/src/components/navigator/AppNavigator.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { StackNavigator } from 'react-navigation'
 
-import Home from './screens/home/Home'
 import Welcome from './screens/welcome/Welcome'
+import Home from './screens/home/Home'
 import Top5 from './screens/top5/Top5'
+import Contacts from './screens/contacts/Contacts'
 import Groups from './screens/groups/Groups'
 
 
@@ -17,6 +18,9 @@ const AppNavigator = StackNavigator(
     },
     Top5: {
       screen: Top5
+    },
+    Contacts: {
+      screen: Contacts
     },
     Groups: {
       screen: Groups
