ea606814b529f7ae612a07fa965b65f822cb33af
tdostilio <tdostilio@gmail.com>
2017-10-04 13:23:59 -0400

    added chris's files

---
+++ Otto/ios/ContactManager.h
@@ -0,0 +1,13 @@
+//
+//  ContactManager.h
+//  AwesomeContact
+//
+//  Created by Chris Aquino on 2017-10-04.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface ContactManager : NSObject <RCTBridgeModule>
+
+@end
