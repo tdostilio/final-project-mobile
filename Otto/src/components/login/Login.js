c52ce5d3fc540f512631ed0fd6a401aa102dacd6
James Hong <gnohj21@gmail.com>
2017-10-04 12:30:43 -0400

    add Login

--- Otto/src/components/login/Login.js
+++ Otto/src/components/login/Login.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Image, KeyboardAvoidingView } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
+
+import mainlogo from '../../static/images/mainlogo.png'
+import LoginForm from './LoginForm'
 
 
 export default class Login extends Component {
@@ -10,19 +14,24 @@ export default class Login extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView behavior='padding' style={styles.container}>
 
         <View style={styles.logoContainer}>
-          <Text>
-            Login Screen
+          <Image
+            style={styles.logo}
+            source={mainlogo}
+          />
+          <Text
+            style={styles.title}>
+            Otto
           </Text>
         </View>
 
         <View style={styles.formContainer}>
-          
+          <LoginForm />
         </View>
 
-      </View>
+      </KeyboardAvoidingView>
     )
   }
 }
@@ -31,12 +40,21 @@ export default class Login extends Component {
 const styles = {
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: 'dodgerblue'
   },
   logoContainer: {
-
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center'
+  },
+  logo: {
+    width: 150,
+    height: 150
+  },
+  title: {
+    color: '#eee',
+    fontSize: 35,
+    marginTop: 10
   },
   formContainer: {
 
