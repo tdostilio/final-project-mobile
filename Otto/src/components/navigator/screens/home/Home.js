1231a1c5f7d1a6cb864c6d09fd14ff1cb2d4ea5e
James Hong <gnohj21@gmail.com>
2017-10-04 19:11:04 -0400

    add refactored Header Component

--- Otto/src/components/navigator/screens/home/Home.js
+++ Otto/src/components/navigator/screens/home/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { ActivityIndicator, ListView, Button, Text, View, Image } from 'react-native'
 
+import Header from '../../util/Header'
+
 
 export default class Home extends Component {
 
@@ -12,24 +14,7 @@ export default class Home extends Component {
     const { navigate } = this.props.navigation
     return (
       <View>
-        <Button
-        title="Go to Welcome"
-        onPress={() =>
-            navigate('Welcome')
-        }
-        />
-        <Button
-        title="Go to Top 5"
-        onPress={() =>
-        navigate('Top5')
-        }
-        />
-        <Button
-        title="Go to Groups"
-        onPress={() =>
-            navigate('Groups')
-        }
-        />
+      <Header navigate={navigate}/>
         <Image
           source={{uri: 'https://vignette2.wikia.nocookie.net/ghostbusters/images/1/11/Bill_Murray.jpeg/revision/latest?cb=20081013140955'}}
           style={{height: 500, width: 500, borderWidth: 1, borderColor: 'red' }}
