6ee54eff420640270e193a96df343518e0f69a28
William Elliott <wcelliott1991@gmail.com>
2017-10-04 11:13:52 -0400

    redesign of navigation bar on welcome screen

--- Otto/src/components/unique/Welcome.js
+++ Otto/src/components/unique/Welcome.js
@@ -10,29 +10,32 @@ class Welcome extends React.Component {
       const { navigate } = this.props.navigation;
       return (
         <View>
+            <View style={styles.navStyle}>
+                <Button
+                title="Go to Home"
+                onPress={() =>
+                    navigate('Home')
+                }
+                />
+                <Button
+                title="Go to Top 5"
+                onPress={() =>
+                navigate('Top5')
+                }
+                />
+                <Button
+                title="Go to Groups"
+                onPress={() =>
+                    navigate('Groups')
+                }
+                />
+            </View>
             <Image
             source={require('../images/mainlogo.png')}
             style={styles.logoStyle}
             />
             <Button
-            title="Go to Home"
-            onPress={() =>
-                navigate('Home')
-            }
-            />
-            <Button
-            title="Go to Top 5"
-            onPress={() =>
-            navigate('Top5')
-            }
-            />
-            <Button
-            title="Go to Groups"
-            onPress={() =>
-                navigate('Groups')
-            }
-            />
-            <Button
+                style={styles.startStyle}
                 title='Lets Get Started!'
                  onPress={() =>
                 navigate('Who')
@@ -49,6 +52,15 @@ const styles = {
     width: 200, 
     alignSelf: 'center', 
     justifyContent: 'center',
+    marginTop: 75,
+    marginBottom: 75
+    },
+    navStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    startStyle: {
+        fontSize: 24
     }
 }
 
