1b3913e9b37c4561de82eaeca82b3c22873445ed
James Hong <gnohj21@gmail.com>
2017-10-05 15:22:50 -0400

    changed dir path

---
+++ Otto/src/components/screens/groups/GroupList.js
@@ -0,0 +1,20 @@
+import { CardSection } from '../../../components/util';
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+
+class GroupList extends Component {
+    renderContacts = (contacts) => {
+        return contacts.map( (item, key) => {
+            return <CardSection contact={item} key={key}/>
+        })
+    }
+    render() {
+        return (
+            <ScrollView>
+                {this.renderContacts(this.props.contacts)}
+            </ScrollView>
+        );
+    }
+}
+
+export default GroupList;
