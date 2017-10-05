d39bf0a309d05c454ecdc7dc5721a5c54164679b
James Hong <gnohj21@gmail.com>
2017-10-05 15:21:26 -0400

    update styling for ContactList

---
+++ Otto/src/components/screens/contacts/ContactList.js
@@ -0,0 +1,32 @@
+import { CardSection } from '../../../components/util';
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+
+class ContactList extends Component {
+    renderContacts = (contacts) => {
+        return contacts.map( (item, key) => {
+            return <CardSection style={styles.ContactStyle} contact={item} key={key}/>
+        })
+    }
+    render() {
+        return (
+            <ScrollView style={styles.scrollContainer}>
+                {this.renderContacts(this.props.contacts)}
+            </ScrollView>
+        );
+    }
+}
+
+styles = {
+    scrollContainer: {
+        backgroundColor: `#C5E4DB`
+    },
+    ContactStyle: {
+        alignSelf: 'center',
+        borderWidth: 1,
+        borderColor: 'red',
+        backgroundColor: `#C5E4DB`
+    }
+}
+
+export default ContactList;
