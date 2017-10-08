d945d9b33bbca05be5c6804db4b5f177cafe0bf3
James Hong <gnohj21@gmail.com>
2017-10-08 04:45:00 -0400

    add contacts2

---
+++ Otto/src/components/screens/contacts2/Contacts2.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import {
+  Text,
+  View,
+  ScrollView
+} from 'react-native'
+import { List, ListItem, SearchBar } from 'react-native-elements'
+import { users } from '../../router/data'
+
+export default class Contacts2 extends Component {
+
+  state = {
+    search: ''
+  }
+
+  sortArray = (array, text) => {
+    return array.filter( (person) => {
+        return person.givenName.search(text) !== -1;
+    });
+  }
+
+  handleSearch = (text) => {
+    this.setState({search: text}, () => {
+        if (this.state.search.length === 0) {
+            this.setState({searching: null})
+        }
+        this.setState({searching: this.sortArray(this.state.contacts, this.state.search)})
+    })
+  }
+
+  onLearnMore = (user) => {
+    this.props.navigation.navigate('Contacts2Detail', { ...user })
+  }
+
+  render() {
+    return (
+      <ScrollView style={{backgroundColor: `#222A43`}}>
+                  <View>
+                <SearchBar
+                    round
+                    inputStyle={{color: '#1EBF9E', backgroundColor: 'black'}}
+                    containerStyle={{backgroundColor: 'black'}}
+                    onChangeText={this.handleSearch}
+                    value={this.state.search}
+                    placeholder='Search ...'
+                />
+            </View>
+        <List>
+          {users.map((user) => (
+            <ListItem
+              key={user.login.username}
+              roundAvatar
+              avatar={{ uri: user.picture.thumbnail }}
+              title={`${user.name.first.toUpperCase()} ${user.name.last.toUpperCase()}`}
+              subtitle={user.email}
+              subtitleStyle={{color: '#1EBF9E'}}
+              onPress={() => this.onLearnMore(user)}
+              titleStyle={{color: '#eee'}}
+              containerStyle={{backgroundColor: `#222A43`}}
+              chevronColor={'#5D8DAD'}
+            />
+          ))}
+        </List>
+      </ScrollView>
+    )
+  }
+}
