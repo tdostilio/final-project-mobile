2eb6a03753df2d774a4790f4331762dd60a55383
James Hong <gnohj21@gmail.com>
2017-10-07 22:09:59 -0400

    fix syntax

--- Otto/src/components/screens/Feed.js
+++ Otto/src/components/screens/Feed.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Text,
   View,
   ScrollView
-} from 'react-native';
-import { List, ListItem } from 'react-native-elements';
-import { users } from '../router/data';
+} from 'react-native'
+import { List, ListItem } from 'react-native-elements'
+import { users } from '../router/data'
 
-class Feed extends Component {
+export default class Feed extends Component {
   onLearnMore = (user) => {
-    this.props.navigation.navigate('Details', { ...user });
-  };
+    this.props.navigation.navigate('Details', { ...user })
+  }
 
   render() {
     return (
-      <ScrollView>
+      <ScrollView style={{backgroundColor: `#222A43`}}>
         <List>
           {users.map((user) => (
             <ListItem
@@ -31,8 +31,6 @@ class Feed extends Component {
           ))}
         </List>
       </ScrollView>
-    );
+    )
   }
 }
-
-export default Feed;
