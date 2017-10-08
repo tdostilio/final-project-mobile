c49ba4cd569cafeef085677578e044d24b1eadfd
James Hong <gnohj21@gmail.com>
2017-10-08 04:41:00 -0400

    update Router

--- Otto/src/components/router/Router.js
+++ Otto/src/components/router/Router.js
@@ -4,11 +4,17 @@ import { Icon } from 'react-native-elements'
 
 // Me Screen
 import Me from '../screens/me/Me'
-import Settings from '../screens/me/Settings'
+import RecentCall from '../screens/me/RecentCall'
+import RecentText from '../screens/me/RecentText'
+import RecentEmail from '../screens/me/RecentEmail'
 
 // Feed Screen
 import Feed from '../screens/feed/Feed'
-import UserDetail from '../screens/feed/UserDetail'
+import AddReminder from '../screens/feed/AddReminder'
+
+// Contacts2 Screen
+import Contacts2 from '../screens/contacts2/Contacts2'
+import Contacts2Detail from '../screens/contacts2/Contacts2Detail'
 
 // Contact Screen
 import Contacts from '../screens/contacts/Contacts'
@@ -16,18 +22,55 @@ import Contacts from '../screens/contacts/Contacts'
 // Groups Screen
 import Groups from '../screens/groups/Groups'
 
+// Settings Screen
+import Settings from '../screens/settings/Settings'
+
+
+export const MeStack = StackNavigator({
+  Me: {
+    screen: Me,
+    navigationOptions: {
+      title: 'Welcome',
+      headerStyle: {
+        backgroundColor: `#222A43`
+      },
+      headerTintColor: '#eee'
+    },
+  }
+})
 
 export const FeedStack = StackNavigator({
   Feed: {
     screen: Feed,
     navigationOptions: {
       title: 'Feed',
+      headerStyle: {
+        backgroundColor: `#222A43`
+      },
+      headerTintColor: '#eee'
+    },
+  }
+})
+
+export const Contacts2Stack = StackNavigator({
+  Contacts2: {
+    screen: Contacts2,
+    navigationOptions: {
+      title: 'Contacts2',
+      headerStyle: {
+        backgroundColor: `#222A43`
+      },
+      headerTintColor: '#fff'
     },
   },
-  Details: {
-    screen: UserDetail,
+  Contacts2Detail: {
+    screen: Contacts2Detail,
     navigationOptions: ({ navigation }) => ({
       title: `${navigation.state.params.name.first.toUpperCase()} ${navigation.state.params.name.last.toUpperCase()}`,
+      headerStyle: {
+        backgroundColor: `#222A43`
+      },
+      headerTintColor: '#fff'
     }),
   },
 })
@@ -37,6 +80,10 @@ export const ContactsStack = StackNavigator({
     screen: Contacts,
     navigationOptions: {
       title: 'Contacts',
+      headerStyle: {
+        backgroundColor: `#222A43`
+      },
+      headerTintColor: '#fff'
     },
   }
 })
@@ -46,6 +93,10 @@ export const GroupsStack = StackNavigator({
     screen: Groups,
     navigationOptions: {
       title: 'Groups',
+      headerStyle: {
+        backgroundColor: `#222A43`
+      },
+      headerTintColor: '#fff'
     },
   }
 })
@@ -55,13 +106,17 @@ export const SettingsStack = StackNavigator({
     screen: Settings,
     navigationOptions: {
       title: 'Settings',
+      headerStyle: {
+        backgroundColor: `#222A43`
+      },
+      headerTintColor: '#fff'
     },
   },
 })
 
 export const Tabs = TabNavigator({
   Me: {
-    screen: Me,
+    screen: MeStack,
     navigationOptions: {
       tabBarLabel: 'Me',
       tabBarIcon: ({ tintColor }) => <Icon name="account-box" size={30} color={tintColor} />
@@ -71,7 +126,7 @@ export const Tabs = TabNavigator({
     screen: FeedStack,
     navigationOptions: {
       tabBarLabel: 'Feed',
-      tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />
+      tabBarIcon: ({ tintColor }) => <Icon name="list" size={30} color={tintColor} />
     },
   },
   Contacts: {
@@ -81,15 +136,28 @@ export const Tabs = TabNavigator({
       tabBarIcon: ({ tintColor }) => <Icon name="import-contacts" size={34} color={tintColor} />
     }
   },
+  Contacts2: {
+    screen: Contacts2Stack,
+    navigationOptions: {
+      tabBarLabel: 'Contact2',
+      tabBarIcon: ({ tintColor }) => <Icon name="import-contacts" size={34} color={tintColor} />
+    },
+  },
   Groups: {
     screen: GroupsStack,
       navigationOptions: {
         tabBarLabel: 'Groups',
         tabBarIcon: ({ tintColor }) => <Icon name="group-work" size={30} color={tintColor} />
       }
+  },
+  Settings: {
+    screen: SettingsStack,
+    navigationOptions: {
+      tabBarLabel: 'Settings',
+      tabBarIcon: ({ tintColor }) => <Icon name="settings" size={30} color={tintColor} />
+    }
   }
-},
-{
+},{
   tabBarPosition: 'bottom',
   tabBarComponent: TabBarBottom,
   animationEnabled: true,
@@ -102,7 +170,7 @@ export const Tabs = TabNavigator({
     indicatorStyle: {
       borderBottomColor: 'green'
     },
-    activeBackgroundColor: '#1EBF9E',
+    activeBackgroundColor: '#5D8DAD',
     upperCaseLabel: true,
     activeTintColor: '#fff',
     inactiveTintColor: '#fff',
@@ -123,12 +191,74 @@ export const Tabs = TabNavigator({
   }
 })
 
+// isolated stacks / rely on its own redirect to go back
+export const RecentCallStack = StackNavigator({
+  RecentCall: {
+    screen: RecentCall,
+    navigationOptions: {
+      title: 'Recent Calls',
+      headerStyle: {
+        backgroundColor: `#222A43`
+      },
+      headerTintColor: '#fff'
+    }
+  }
+})
+
+export const RecentTextStack = StackNavigator({
+  RecentText: {
+    screen: RecentText,
+    navigationOptions: {
+      title: 'Recent Texts',
+      headerStyle: {
+        backgroundColor: `#222A43`
+      },
+      headerTintColor: '#fff'
+    }
+  }
+})
+
+export const RecentEmailStack = StackNavigator({
+  RecentEmail: {
+    screen: RecentEmail,
+    navigationOptions: {
+      title: 'Recent Email',
+      headerStyle: {
+        backgroundColor: `#222A43`
+      },
+      headerTintColor: '#fff'
+    }
+  }
+})
+
+export const AddReminderStack = StackNavigator({
+  AddReminder: {
+    screen: AddReminder,
+    navigationOptions: {
+      title: 'Add A Reminder',
+      headerStyle: {
+        backgroundColor: `#222A43`
+      },
+      headerTintColor: '#fff'
+    }
+  }
+})
+
 export const Root = StackNavigator({
   Tabs: {
     screen: Tabs,
   },
-  Settings: {
-    screen: SettingsStack,
+  RecentCall: {
+    screen: RecentCallStack,
+  },
+  RecentText: {
+    screen: RecentTextStack,
+  },
+  RecentEmail: {
+    screen: RecentEmailStack,
+  },
+  AddReminder: {
+    screen: AddReminderStack,
   },
 }, {
   mode: 'modal',
