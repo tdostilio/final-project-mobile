853ad00285963fe26b2f65685df098c20b3432cf
James Hong <gnohj21@gmail.com>
2017-10-07 22:03:02 -0400

    change syntax

---
+++ Otto/src/components/screens/Feed.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  ScrollView
+} from 'react-native';
+import { List, ListItem } from 'react-native-elements';
+import { users } from '../router/data';
+
+class Feed extends Component {
+  onLearnMore = (user) => {
+    this.props.navigation.navigate('Details', { ...user });
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <List>
+          {users.map((user) => (
+            <ListItem
+              key={user.login.username}
+              roundAvatar
+              avatar={{ uri: user.picture.thumbnail }}
+              title={`${user.name.first.toUpperCase()} ${user.name.last.toUpperCase()}`}
+              subtitle={user.email}
+              onPress={() => this.onLearnMore(user)}
+              titleStyle={{color: '#eee'}}
+              containerStyle={{backgroundColor: `#222A43`}}
+              chevronColor={'#1EBF9E'}
+            />
+          ))}
+        </List>
+      </ScrollView>
+    );
+  }
+}
+
+export default Feed;
