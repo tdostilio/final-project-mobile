8e1410134f116f6b5555c4b9ea543e7ae8114649
James Hong <gnohj21@gmail.com>
2017-10-09 02:48:14 -0400

    moved components to another dir

---
+++ Otto/src/components/login/SignUp.js
@@ -0,0 +1,172 @@
+import React, { Component } from 'react'
+import {AsyncStorage, StyleSheet, Text, View, Button } from 'react-native'
+import t from 'tcomb-form-native'
+
+import url from '../util/api/config'
+
+
+const Form = t.form.Form
+
+const User = t.struct({
+  firstName: t.String,
+  lastName: t.String,
+  email: t.String,
+  password: t.String,
+  confirmPassword: t.String,
+  rememberMe: t.Boolean
+});
+
+const formStyles = {
+  ...Form.stylesheet,
+  formGroup: {
+    normal: {
+      marginBottom: 10
+    },
+  },
+  controlLabel: {
+    normal: {
+      color: '#222A43',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    },
+    // the style applied when a validation error occours
+    error: {
+      color: 'red',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    }
+  }
+}
+
+const options = {
+  auto: 'placeholders',
+  fields: {
+    firstName: {
+      autoCorrect: false,
+      error: 'Enter a first name'
+    },
+    lastName: {
+      autoCorrect: false,
+      error: 'Enter a last name'
+    },
+    email: {
+      keyboardType: 'email-address',
+      autoCorrect: false,
+      error: 'Enter an email'
+    },
+    password: {
+      password: true,
+      secureTextEntry: true,
+      autoCorrect: false,
+      error: 'Enter a password'
+    },
+    confirmPassword: {
+      password: true,
+      autoCorrect: false,
+      secureTextEntry: true,
+      placeHolder: 'Confirm Password'
+    },
+    rememberMe: {
+      placeHolder: 'Remember Me'
+    }
+  },
+  stylesheet: formStyles,
+};
+
+export default class SignUpForm extends Component {
+
+  state = {
+    passwordError: false,
+    duplicateEmail: false
+  }
+
+  saveItem = async (item, selectedValue) => {
+    try {
+      await AsyncStorage.setItem(item, selectedValue);
+    } catch (error) {
+      console.error('AsyncStorage error: ' + error.message);
+    }
+  }
+
+  userSignup = (user) => {
+    fetch(url.CREATE_ACCOUNT, {
+      method: 'POST',
+      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        password: user.password
+      })
+    })
+    .then(response => response.json())
+    .then(responseData => {
+      if (responseData.error) {
+        this.setState({duplicateEmail: true})
+        return;
+      } else {
+        console.log(responseData)
+        
+        // uncomment when ready to save to AsyncStorage
+        // this.saveItem('id_token', responseData.id_token)
+        return true;
+      }
+    })
+    .done();
+  }
+
+  handleSubmit = () => {
+    const user = this._form.getValue();
+    if (!(user)) return null;
+
+
+    if (this.checkPassword(user)) {
+     let tokenSuccess =  this.userSignup(user)
+      if (!(tokenSuccess)) {
+        return;
+      }
+      this.props.navigation.navigate('SignedIn', )
+    } else {
+      this.setState({passwordError: true})
+    }
+  }
+
+  checkPassword = (user) => {
+    if ( (user.password) === (user.confirmPassword) ) {
+      return true
+    } else {
+      return false
+    }
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Form 
+          ref={c => this._form = c}
+          type={User} 
+          options={options}
+        />
+        {this.state.passwordError ? <Text style={styles.warning}> Correct passwords required </Text> : null}
+        {this.state.duplicateEmail ? <Text style={styles.warning}> Email already registered </Text> : null}
+        <Button
+          title="Sign Up!"
+          onPress={this.handleSubmit}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  warning: {
+    color: 'red'
+  }
+})
