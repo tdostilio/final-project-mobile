9e0cb4c937419e97adad2a208594549ecdfce670
James Hong <gnohj21@gmail.com>
2017-10-08 23:41:53 -0400

    use tcomb form for createAccount

--- Otto/src/components/login/CreateAccount.js
+++ Otto/src/components/login/CreateAccount.js
@@ -1,26 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import { View } from "react-native";
-import { Card, Button, FormLabel, FormInput } from "react-native-elements";
+import { Card, Button, FormLabel, FormInput, FormValidationMessage } from "react-native-elements";
 import { onSignIn } from "../auth/Auth";
 
-export default ({ navigation }) => (
-  <View style={{ flex: 1, backgroundColor: '#222A43', paddingVertical: 20 }}>
-    <Card containerStyle={{backgroundColor: '#b6ccda'}}>
-      <FormLabel labelStyle={{color: '#222A43'}}>Email</FormLabel>
-      <FormInput inputStyle={{color: '#222A43'}} placeholder="Email address..." />
-      <FormLabel labelStyle={{color: '#222A43'}}>Password</FormLabel>
-      <FormInput inputStyle={{color: '#222A43'}} secureTextEntry placeholder="Password..." />
-      <FormLabel labelStyle={{color: '#222A43'}}>Confirm Password</FormLabel>
-      <FormInput inputStyle={{color: '#222A43'}} secureTextEntry placeholder="Confirm Password..." />
+import SignUpForm from '../util/Form/SignUp'
 
-      <Button
-        buttonStyle={{ marginTop: 20 }}
-        backgroundColor="#03A9F4"
-        title="Sign Up"
-        onPress={() => {
-          onSignIn().then(() => navigation.navigate("SignedIn"));
-        }}
-      />
-    </Card>
-  </View>
-);
+
+export default class CreateAccount extends Component {
+  
+  render() {
+    const { navigation } = this.props
+    return (
+      <View style={{ flex: 1, backgroundColor: '#222A43', paddingVertical: 20 }}>
+        <Card containerStyle={{backgroundColor: '#b6ccda'}}>
+          <SignUpForm navigation={navigation}/>
+        </Card>
+       </View>
+    );
+  }
+}
