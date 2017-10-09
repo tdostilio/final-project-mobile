7b9a95dea9267b1b54d3270753e57af6af5361b4
James Hong <gnohj21@gmail.com>
2017-10-09 02:47:47 -0400

    refactor

--- Otto/src/components/login/CreateAccount.js
+++ Otto/src/components/login/CreateAccount.js
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
-import { View } from "react-native";
-import { Card, Button, FormLabel, FormInput, FormValidationMessage } from "react-native-elements";
-import { onSignIn } from "../auth/Auth";
+import React from "react"
+import { View } from "react-native"
+import { Card } from "react-native-elements"
+import { onSignIn } from "../auth/Auth"
 
-import SignUpForm from '../util/Form/SignUp'
+import SignUpForm from './SignUp'
 
 
-export default class CreateAccount extends Component {
-  
-  render() {
-    const { navigation } = this.props
-    return (
-      <View style={{ flex: 1, backgroundColor: '#222A43', paddingVertical: 20 }}>
-        <Card containerStyle={{backgroundColor: '#b6ccda'}}>
-          <SignUpForm navigation={navigation}/>
-        </Card>
-       </View>
-    );
-  }
-}
+const CreateAccount = ({ navigation }) => (
+  <View style={{ flex: 1, backgroundColor: '#222A43', paddingVertical: 20 }}>
+    <Card containerStyle={{backgroundColor: '#b6ccda'}}>
+      <SignUpForm navigation={navigation}/>
+    </Card>
+  </View>
+)
+
+export default CreateAccount
