5b05d8ed528a8a68d4ff46a592fbf950358a8dfe
tdostilio <tdostilio@gmail.com>
2017-10-10 11:51:57 -0400

    installed vector icons

--- Otto/src/components/screens/contacts/ContactCard.js
+++ Otto/src/components/screens/contacts/ContactCard.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import Communications from 'react-native-communications';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 const ContactCard = (props) => {
     wrapText = (info) => {
         return <Text style={styles.textStyle}>{info}</Text>
     }
     return (
-        <TouchableOpacity onPress={() => Communications.phonecall(props.contact.phoneNumbers[0]['number'], true)}>
+        <TouchableOpacity 
+            onPress={() => Communications.phonecall(props.contact.phoneNumbers[0]['number'], true)}
+
+        >
             <View style={styles.containerStyle}>
             {
                 props.contact ? 
                 wrapText(props.contact.givenName.toString()+ " " +props.contact.familyName.toString())
                 :wrapText('No Contacts Found')
             }
+            <Icon name="angle-right" style={styles.chevronStyle} />
             </View>
         </TouchableOpacity>
     );
 };
 
 const styles = {
+    testStyle: {
+        alignItems: 'space-between'
+    },
     containerStyle: {
+        alignItems: 'space-between',
         borderBottomWidth: 1,
         padding: 10,
         backgroundColor: '#222A43',
@@ -31,7 +41,18 @@ const styles = {
     textStyle: {
         fontSize: 20,
         color: '#5D8DAD'
+    },
+    chevronStyle: {
+        justifyContent: 'center',
+        marginLeft: 200,
+        size: 24,
+        color: "#C8C7CC" 
     }
+
 };
+
+
+// Communications code for calling:
+// onPress={() => Communications.phonecall(props.contact.phoneNumbers[0]['number'], true)}
 
 export { ContactCard };
