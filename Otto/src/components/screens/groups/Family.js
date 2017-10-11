bc0c4c8a7d56f5d6dc2ee51361305dba130c2253
James Hong <gnohj21@gmail.com>
2017-10-11 17:33:28 -0400

    first commit

---
+++ Otto/src/components/screens/groups/Family.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { View, ScrollView, Text } from 'react-native';
+import { List, ListItem, Button, Icon } from 'react-native-elements';
+
+
+export default class Family extends Component {
+  state = {
+
+  }
+
+  componentDidMount() {
+    // make ajax call to hydrate this state.. do it here or from `Me Component` and pass down as props
+    // change loading to false once state hydrated.. for development-- leave it true
+  }
+
+  
+  render() {
+    const { navigate } = this.props.navigation
+
+    return (
+      <View style={styles.container}>
+
+        <ScrollView>
+        <Text>Family Main Body</Text>
+        </ScrollView>
+
+      </View>
+    )
+  }
+}
+
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: `#222A43`
+  }
+}
