ae034cd2b255f712e2d91c2f2d4fa2d3be3db425
William Elliott <wcelliott1991@gmail.com>
2017-10-11 12:31:09 -0400

    abstracts reminder options logic and simulates multiple reminders

--- Otto/src/components/screens/feed/Feed.js
+++ Otto/src/components/screens/feed/Feed.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, ScrollView, Text } from 'react-native';
 import { List, ListItem, Icon, Button} from 'react-native-elements';
+import ReminderOptions from './ReminderOptions';
 
 export default class Feed extends Component {
   state = {
@@ -15,19 +16,6 @@ export default class Feed extends Component {
   handleAddReminderPress = () => {
     this.props.navigation.navigate('AddReminder')
   }
-
-  handleYesPress = () => {
-    this.props.navigation.navigate('')
-  }
-  handleNoPress = () => {
-    this.props.navigation.navigate('')
-  }
-  handleCallPress = () => {
-    this.props.navigation.navigate('')
-  }
-  handleTextPress = () => {
-    this.props.navigation.navigate('')
-  }
   
   render() {
     const { navigate } = this.props.navigation
@@ -37,51 +25,10 @@ export default class Feed extends Component {
       <View style={styles.container}>
       
         <ScrollView>
-          <View style={styles.optionsGroupContainer}>
-            <View style={styles.optionsContainer}>
-              <Text style={{ color: "white" }}>Have you talked to [Contact] recently?</Text>
-            </View>
-              <View style={styles.optionsButtonContainer}>
-                <Button
-                borderRadius={75}
-                raised
-                medium
-                buttonStyle={{marginBottom: 5, backgroundColor: `green`}}
-                title='Yes'
-                color='white'
-                onPress={this.handleYesPress} 
-                />
-                <Button
-                borderRadius={75}
-                raised
-                medium
-                buttonStyle={{marginBottom: 5, backgroundColor: `red`}}
-                title='No'
-                color='white'
-                onPress={this.handleNoPress} 
-                />
-                <Button
-                borderRadius={75}
-                raised
-                medium
-                buttonStyle={{marginBottom: 5, backgroundColor: `#5D8DAD`}}
-                icon={{name: 'phone', type: 'material', color: 'white'}}
-                title='Call'
-                color='white'
-                onPress={this.handleCallPress} 
-                />
-                <Button
-                borderRadius={75}
-                raised
-                medium
-                buttonStyle={{marginBottom: 5, backgroundColor: `#5D8DAD`}}
-                icon={{name: 'smartphone', type: 'material', color: 'white'}}
-                title='Text'
-                color='white'
-                onPress={this.handleTextPress} 
-                />
-              </View>
-            </View>
+          <ReminderOptions/>
+          <ReminderOptions/>
+          <ReminderOptions/>
+
         </ScrollView>
       
         <View style={styles.buttonContainer}>
@@ -113,26 +60,4 @@ const styles = {
     flexDirection: 'row-reverse',
     margin: 20
   },
-  optionsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 5
-  },
-  optionsButtonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 5,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    elevation: 2,
-  },
-  optionsGroupContainer: {
-    borderWidth: 1,
-    borderColor: '#5D8DAD',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    elevation: 2
-  }
 }
