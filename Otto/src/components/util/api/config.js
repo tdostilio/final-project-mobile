1d07ee450ab643bb068f6ab742ce8f21bfd0b150
James Hong <gnohj21@gmail.com>
2017-10-12 12:54:30 -0400

    add comments for endpoints

--- Otto/src/components/util/api/config.js
+++ Otto/src/components/util/api/config.js
@@ -3,4 +3,8 @@ export default {
   CREATE_ACCOUNT: 'http://localhost:3000/signup',
   LOGIN_ACCOUNT: 'http://localhost:3000/signin',
   CREATE_GROUP: (endpoint, id) => `http://localhost:3000/${endpoint}/:${id}`
+  // endpoints for create_group (adding contact to a group)
+  // family
+  // close_friends
+  // friends
 }
