ecf88faf27ec8608e6daf2ce4cbdbc14db214712
James Hong <gnohj21@gmail.com>
2017-10-12 16:54:56 -0400

    fix url

--- Otto/src/components/util/api/config.js
+++ Otto/src/components/util/api/config.js
@@ -2,7 +2,7 @@ export default {
   USER_INFO: 'userInfo',
   CREATE_ACCOUNT: 'http://localhost:3000/signup',
   LOGIN_ACCOUNT: 'http://localhost:3000/signin',
-  CREATE_GROUP: (endpoint, id) => `http://localhost:3000/${endpoint}/:${id}`
+  CREATE_GROUP: (endpoint, id) => `http://localhost:3000/${endpoint}/${id}`
   // endpoints for create_group (adding contact to a group)
   // family
   // close_friends
