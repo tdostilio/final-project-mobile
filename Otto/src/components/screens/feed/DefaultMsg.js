f55dfa021123946c51fe632d527eb8ab88b09601
James Hong <gnohj21@gmail.com>
2017-10-15 19:27:51 -0400

    fix refresh logic and styling for defaultmsg component

--- Otto/src/components/screens/feed/DefaultMsg.js
+++ Otto/src/components/screens/feed/DefaultMsg.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { View, Text, RefreshControl } from 'react-native'
-import { Icon } from 'react-native-elements'
+import { View, FlatList, ScrollView, Text, RefreshControl } from 'react-native'
+import { List, Card, Icon, Button} from 'react-native-elements'
+import uuid from 'uuid'
 
 
 const styles = {
   container: {
-    padding: 5,
+    padding: 15,
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    marginBottom: 15,
-    borderRadius: 8
+    justifyContent: 'center',
+    marginBottom: 10,
+    backgroundColor: '#001a33',
+    borderRadius: 8,
+    borderColor: '#001a33',
+    overflow: 'hidden'
+  },
+  cardTitle: {
+    color: `#fff`
   },
   buttonContainer: {
-    width: null,
-    height: null,
-    borderRadius: 80,
-    margin: 20,
-    flexDirection: 'column',
+    flexDirection: 'row',
+    marginBottom: 10,
     justifyContent: 'center'
   }
 }
@@ -30,35 +35,37 @@ DefaultMsg.propTypes = {
 
 export default function DefaultMsg ({refreshing, onRefresh}) {
   return (
-    <View
-      refreshControl={
-        <RefreshControl
-          refreshing={refreshing}
-          onRefresh={onRefresh}
-        />
-      }
-      style={styles.container}>
-      <View>
-        <Text style={{color: 'white'}}>
-          Pull Down To Refresh
-        </Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <Icon
-          raised={true}
-          name='refresh'
-          underlayColor='#001a33'
-          type='material'
-          color='#1E90FF'
-          reverse={true}
-          reverseColor='#fff'
-        />
-      </View>
-      <View>
-        <Text style={{color: 'white'}}>
-          Add some contacts to a group!
-        </Text>
-      </View>
-    </View>
+    <FlatList
+      data={[{}]}
+      refreshing={refreshing}
+      onRefresh={onRefresh}
+      keyExtractor={() => uuid.v4()}
+      renderItem={({item}) => (
+        <Card
+          title={`Pull Down To Refresh`}
+          titleStyle={styles.cardTitle}
+          containerStyle={styles.container}>
+
+            <View style={styles.buttonContainer}>
+              <Icon
+                raised={true}
+                name='refresh'
+                underlayColor='#001a33'
+                type='material'
+                color='#1E90FF'
+                reverse={true}
+                reverseColor='#fff'
+              />
+            </View>
+
+            <View>
+              <Text style={{color: 'white'}}>
+                Add some contacts to a group!
+              </Text>
+            </View>
+
+          </Card>
+      )}
+      />
   )
 }
