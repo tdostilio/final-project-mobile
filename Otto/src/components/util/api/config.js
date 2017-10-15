6a14becf9a741ba8338eeaaad591a443f9dc52a3
James Hong <gnohj21@gmail.com>
2017-10-15 01:33:19 -0400

    add config feed macic string

--- Otto/src/components/util/api/config.js
+++ Otto/src/components/util/api/config.js
@@ -1,5 +1,6 @@
 export default {
   USER_INFO: 'userInfo',
+  FEED: 'feed',
   CREATE_ACCOUNT: 'http://localhost:3000/signup',
   LOGIN_ACCOUNT: 'http://localhost:3000/signin',
   CREATE_GROUP: (endpoint, id) => `http://localhost:3000/${endpoint}/${id}`,
