59b694459869f2094937be8c0f822505a4312742
James Hong <gnohj21@gmail.com>
2017-10-15 14:28:14 -0400

    fix ui styling

--- Otto/src/components/screens/feed/ReminderOptions.js
+++ Otto/src/components/screens/feed/ReminderOptions.js
@@ -9,73 +9,77 @@ export default class ReminderOptions extends Component {
   }
 
   componentDidMount() {
-    // make ajax call to hydrate this state.. do it here or from `Me Component` and pass down as props
-    // change loading to false once state hydrated.. for development-- leave it true
+  
   }
 
-//   handleYesPress = () => {
-//     this.props.navigation.navigate('')
-//   }
-//   handleNoPress = () => {
-//     this.props.navigation.navigate('')
-//   }
-//   handleCallPress = () => {
-//     this.props.navigation.navigate('')
-//   }
-//   handleTextPress = () => {
-//     this.props.navigation.navigate('')
-//   }
+  renderCard = (payload) => {
+    return payload.map((x, idx) => {
+      return (
+        <View style={styles.container} key={idx}>
+
+          <View style={styles.optionsContainer}>
+            <Text
+              style={{ color: "white", fontSize: 16 }}>
+              Have you spoke to &nbsp;
+              <Text
+                style={{color: '#1FFFDA', fontWeight: 'bold', fontSize: 14}}>
+                 {x.first_name} {x.last_name
+              }</Text> recently?
+            </Text>
+          </View>
+
+          <View style={styles.optionsButtonContainer}>
+            <Button
+            borderRadius={75}
+            raised
+            medium
+            buttonStyle={{marginBottom: 5, backgroundColor: `green`}}
+            title='Yes'
+            color='white'
+            onPress={this.handleYesPress} 
+            />
+            <Button
+            borderRadius={75}
+            raised
+            medium
+            buttonStyle={{marginBottom: 5, backgroundColor: `red`}}
+            title='No'
+            color='white'
+            onPress={this.handleNoPress} 
+            />
+            <Button
+            borderRadius={75}
+            raised
+            medium
+            buttonStyle={{marginBottom: 5, backgroundColor: `#001a33`}}
+            icon={{name: 'phone', type: 'material', color: 'white'}}
+            title='Call'
+            color='white'
+            onPress={this.handleCallPress} 
+            />
+            <Button
+            borderRadius={75}
+            raised
+            medium
+            buttonStyle={{marginBottom: 5, backgroundColor: `#001a33`}}
+            icon={{name: 'smartphone', type: 'material', color: 'white'}}
+            title='Text'
+            color='white'
+            onPress={this.handleTextPress} 
+            />
+          </View>
+
+        </View>
+      )
+    })
+  }
   
   render() {
-    // const { navigate } = this.props.navigation
-    // const { loading, contactSync, smsSync, callSync, pushNotifications } = this.state
+    const { payload, credentials } = this.props
     return (
-
-        <View style={styles.optionsGroupContainer}>
-            <View style={styles.optionsContainer}>
-              <Text style={{ color: "white", fontSize: 20 }}>Have you talked to <Text style={{color: '#1FFFDA'}}>[Contacts]</Text> recently?</Text>
-            </View>
-              <View style={styles.optionsButtonContainer}>
-                <Button
-                borderRadius={75}
-                raised
-                medium
-                buttonStyle={{marginBottom: 5, backgroundColor: `green`}}
-                title='Yes'
-                color='white'
-                onPress={this.handleYesPress} 
-                />
-                <Button
-                borderRadius={75}
-                raised
-                medium
-                buttonStyle={{marginBottom: 5, backgroundColor: `red`}}
-                title='No'
-                color='white'
-                onPress={this.handleNoPress} 
-                />
-                <Button
-                borderRadius={75}
-                raised
-                medium
-                buttonStyle={{marginBottom: 5, backgroundColor: `#001a33`}}
-                icon={{name: 'phone', type: 'material', color: 'white'}}
-                title='Call'
-                color='white'
-                onPress={this.handleCallPress} 
-                />
-                <Button
-                borderRadius={75}
-                raised
-                medium
-                buttonStyle={{marginBottom: 5, backgroundColor: `#001a33`}}
-                icon={{name: 'smartphone', type: 'material', color: 'white'}}
-                title='Text'
-                color='white'
-                onPress={this.handleTextPress} 
-                />
-              </View>
-            </View>
+      <View style={styles.optionsGroupContainer}>
+        {this.renderCard(payload)}
+      </View>
     )
   }
 }
@@ -83,10 +87,20 @@ export default class ReminderOptions extends Component {
 
 
 const styles = {
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 15,
+    backgroundColor: 'dodgerblue',
+    borderRadius: 8
+  },
   optionsContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
-    marginTop: 5
+    marginTop: 5,
+    marginBottom: 5
   },
   optionsButtonContainer: {
     flexDirection: 'row',
@@ -94,10 +108,7 @@ const styles = {
     marginTop: 5,
   },
   optionsGroupContainer: {
-    backgroundColor: 'dodgerblue',
-    borderWidth: 0.5,
-    borderRadius: 8,
-    borderColor: '#1FFFDA',
+    backgroundColor: `#001a33`,
     elevation: 2,
     margin: 5
   }
