fedd1b49793befb7b8cac08121a718fbc75d90d5
James Hong <gnohj21@gmail.com>
2017-10-15 01:34:01 -0400

    add lifecycle for feed component

--- Otto/src/components/screens/feed/Feed.js
+++ Otto/src/components/screens/feed/Feed.js
@@ -1,17 +1,47 @@
-import React, { Component } from 'react';
-import { View, ScrollView, Text } from 'react-native';
-import { List, ListItem, Icon, Button} from 'react-native-elements';
-import ReminderOptions from './ReminderOptions';
+import React, { Component } from 'react'
+import { AsyncStorage, View, ScrollView, Text } from 'react-native'
+import { List, ListItem, Icon, Button} from 'react-native-elements'
+import axios from 'axios'
+
+import LottieGears from '../../util/LottieGears'
+import ReminderOptions from './ReminderOptions'
+import config from '../../util/api/config'
+
 
 export default class Feed extends Component {
   state = {
-
+    credentials: '',
+    payload: {},
+    payloadStatus: false
   }
 
-  componentDidMount() {
-    // make ajax call to hydrate this state.. do it here or from `Me Component` and pass down as props
-    // change loading to false once state hydrated.. for development-- leave it true
+  async componentDidMount() {
+    try {
+      const credentials = await AsyncStorage.getItem(config.USER_INFO)
+      console.log(credentials)
+      await this.setState({credentials: JSON.parse(credentials)})
+
+      const id = this.state.credentials.id
+      const token = this.state.credentials.token
+      const result = await axios.get(config.GET_GROUP(config.FEED, id),
+      {headers: {"Authorization": "jwt " + token}})
+      console.log('received payload', result.data)
+      await this.setState({payload: result.data.result, payloadStatus: true})
+
+      } catch (error) {
+        console.log(error)
+      }
   }
+
+  singularizeRoute = (route) => {
+    let length = route.length
+    if (route[length-1] === 's') {
+      return route.slice(0, length-2)
+    } else {
+      return route
+    }
+  }
+
 
   handleAddReminderPress = () => {
     this.props.navigation.navigate('AddReminder')
@@ -19,29 +49,27 @@ export default class Feed extends Component {
   
   render() {
     const { navigate } = this.props.navigation
-    const { loading, contactSync, smsSync, callSync, pushNotifications } = this.state
-    return (
+    const { credentials, payloadStatus } = this.state
+    if (!payloadStatus) return <View style={styles.container}><LottieGears /></View>
 
+    return (
       <View style={styles.container}>
       
-
         <ScrollView>
-          <ReminderOptions/>
-          <ReminderOptions/>
-          <ReminderOptions/>
+          <ReminderOptions credentials={credentials}/>
         </ScrollView>
       
         <View style={styles.buttonContainer}>
-        <Icon
-          raised={true}
-          name='add-circle'
-          onPress={this.handleReminderPress}
-          underlayColor='#001a33'
-          type='material'
-          color='#1E90FF'
-          reverse={true}
-          reverseColor='#fff'
-          onPress={this.handleAddReminderPress} />
+          <Icon
+            raised={true}
+            name='add-circle'
+            onPress={this.handleReminderPress}
+            underlayColor='#001a33'
+            type='material'
+            color='#1E90FF'
+            reverse={true}
+            reverseColor='#fff'
+            onPress={this.handleAddReminderPress} />
         </View>
       
       </View>
