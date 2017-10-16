183801db7fa611b9728087ac656941893bc86301
James Hong <gnohj21@gmail.com>
2017-10-16 01:26:10 -0400

    first commit

---
+++ Otto/src/components/util/LottieCheckSuccess.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Animation from 'lottie-react-native'
+
+import anim from '../../../assets/lottie/checkMarkSuccess.json'
+
+
+export default class LottieCheckSuccess extends Component {
+  componentDidMount() {
+    this.animation.play()
+  }
+
+  render() {
+    const { groupName, person } = this.props
+    return (
+        <View style={styles.container}>
+          <Text style={styles.textStyle}>Reminder was successfully set</Text>
+          <View style={styles.container}>
+            <Animation
+            ref={animation => {
+                this.animation = animation
+            }}
+            style={{
+                alignSelf: 'center',
+                width: 200,
+                height: 200,
+                
+            }}
+            loop={false}
+            source={anim}
+            />
+          </View>
+        </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#001a33',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 0,
+    shadowRadius: 0,
+    shadowOffset: {width:0, height: 0}
+  },
+  textStyle: {
+    color: '#5D8DAD',
+    fontSize: 25
+  }
+})
