d30cee94860c3560fd97f7b4683b42b8f9e99102
John Hartert <j.blainehartert@gmail.com>
2017-10-16 17:55:03 -0400

    Adds color for failure message

--- Otto/src/components/util/LottieFailure.js
+++ Otto/src/components/util/LottieFailure.js
@@ -13,7 +13,8 @@ export default class LottieSuccess extends Component {
     const { groupName, person, reason } = this.props
     return (
         <View style={styles.container}>
-          <Text style={styles.textStyle}>{person.givenName} {person.familyName} has already been added to {groupName}.</Text>
+          <Text style={styles.textStyle}>
+          <Text style={{color: '#1FFFDA'}}>{person.givenName} {person.familyName} </ Text> has already been added to {groupName}.</Text>
           <View style={styles.container}>
             <Animation
             ref={animation => {
