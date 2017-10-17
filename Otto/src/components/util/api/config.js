adb01f3293062e45229030d7094b481859d6aa0e
James Hong <gnohj21@gmail.com>
2017-10-17 17:19:34 -0400

    fix host

--- Otto/src/components/util/api/config.js
+++ Otto/src/components/util/api/config.js
@@ -1,9 +1,9 @@
 export default {
   USER_INFO: 'userInfo',
   FEED: 'feed',
-  CREATE_ACCOUNT: 'http://localhost:3000/signup',
-  LOGIN_ACCOUNT: 'http://localhost:3000/signin',
-  CREATE_GROUP: (endpoint, id) => `http://localhost:3000/${endpoint}/${id}`,
-  GET_GROUP: (endpoint, id) => `http://localhost:3000/${endpoint}/${id}`,
-  ADD_REMINDER: (endpoint, id) => `http://localhost:3000/${endpoint}/reminder/${id}`
+  CREATE_ACCOUNT: 'https://myottoapp.com/signup',
+  LOGIN_ACCOUNT: 'https://myottoapp.com/signin',
+  CREATE_GROUP: (endpoint, id) => `https://myottoapp.com/${endpoint}/${id}`,
+  GET_GROUP: (endpoint, id) => `https://myottoapp.com/${endpoint}/${id}`,
+  ADD_REMINDER: (endpoint, id) => `https://myottoapp.com/${endpoint}/reminder/${id}`
 }
