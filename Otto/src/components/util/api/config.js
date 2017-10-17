9fd408757d0d6ecf143424cc3bf1e7298f20a523
James Hong <gnohj21@gmail.com>
2017-10-16 20:30:57 -0400

    set addreminder config

--- Otto/src/components/util/api/config.js
+++ Otto/src/components/util/api/config.js
@@ -4,5 +4,6 @@ export default {
   CREATE_ACCOUNT: 'http://localhost:3000/signup',
   LOGIN_ACCOUNT: 'http://localhost:3000/signin',
   CREATE_GROUP: (endpoint, id) => `http://localhost:3000/${endpoint}/${id}`,
-  GET_GROUP: (endpoint, id) => `http://localhost:3000/${endpoint}/${id}`
+  GET_GROUP: (endpoint, id) => `http://localhost:3000/${endpoint}/${id}`,
+  ADD_REMINDER: (endpoint, id) => `http://localhost:3000/${endpoint}/reminder/${id}`
 }
